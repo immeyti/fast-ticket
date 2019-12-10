d93a6563f8c2285b61834b872d839e0192383533
Mehdi Samirakh <immeyti@Mehdis-MacBook-Air.local>
2019-12-11 02:21:49 +0330

    Initial package

---
+++ src/FastTicketServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Immeyti\FastTicket;
+
+use Illuminate\Support\ServiceProvider;
+
+class FastTicketServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->loadRoutesFrom(__DIR__.'/routes.php');
+    }
+}
---
+++ src/routes.php
@@ -0,0 +1,5 @@
+<?php
+
+Route::get('/foo', function () {
+    return 'this is fast ticket';
+});
