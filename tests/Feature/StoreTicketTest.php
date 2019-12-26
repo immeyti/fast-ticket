be4857db04188e0097611587ef1edfc44ab060f7
Immeyti <mehdi.samirakh68@gmail.com>
2019-12-26 15:35:35 +0330

    Added ticket model and migration

--- tests/Feature/StoreTicketTest.php
+++ tests/Feature/StoreTicketTest.php
@@ -2,20 +2,30 @@
 
 namespace Immeyti\FastTicket\Tests\Feature;
 
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Immeyti\FastTicket\Models\Ticket;
 use Tests\TestCase;
 
-class ExampleTest extends TestCase
+class StoreTicketTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testBasicTest()
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
-        $response->assertStatus(200);
+    /** @test  */
+    public function a_customer_can_create_a_ticket()
+    {
+        $attr = [
+            'title' => 'server problem',
+            'description' => 'this is my desc for ticket',
+            'priority' => 1,
+        ];
+
+        $response = $this->json('post', '/tickets', $attr);
+
+        $this->assertDatabaseHas('tickets', $attr);
+        $response->assertStatus(201)
+            ->assertJson([
+                'created' => true
+            ]);
     }
 }
